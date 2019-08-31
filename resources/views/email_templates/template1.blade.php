37f8a49d2b13c8da7caff3e0f82222f10d80743e
sabeeh sultan <Shahzaiibimran@gmail.com>
2019-08-31 17:26:34 +0500

    31-08-19 --03

--- resources/views/email_templates/template1.blade.php
+++ resources/views/email_templates/template1.blade.php
@@ -576,7 +576,7 @@
                         </tr>
                         <tr>
                             <td>
-                                Message : T{{ $message }}
+                                Message : {{ $message }}
                             </td> 
                         </tr>
                     </table>
